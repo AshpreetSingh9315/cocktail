41d4978b228d405d55856030f395cc97df1f3507
Ash Singh <ashpreetsingh9212@gmail.com>
2025-06-21 00:22:33 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <>
+      <main>
+        <Navbar />
+        <Hero />
+        <div className="h-[100vh] w-full bg-black"></div>
+      </main>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,46 @@
+import { useGSAP } from "@gsap/react";
+import { navLinks } from "../Constants";
+import gsap from "gsap";
+
+const Navbar = () => {
+
+    useGSAP(()=>{
+        const timeline = gsap.timeline({
+            scrollTrigger:{
+                trigger : "nav",
+                start : "bottom top",
+
+            }
+        });
+        
+        timeline.fromTo("nav",{
+            backgroundColor : "transparent"
+        },{
+            backgroundColor : "#000000050",
+            backgroundFilter : "blur(10px)",
+            duration : 1,
+            ease : "power1.inOut"
+        })
+    },[])
+
+  return (
+    <nav className="lg:px-10">
+	 <div>
+		<a href="#home" className="flex items-center gap-2">
+		 <img src="/images/logo.png" alt="logo" />
+		 <p>Velvet Pour</p>
+		</a>
+		
+		<ul>
+		 {navLinks.map((link) => (
+			<li key={link.id}>
+			 <a href={`#${link.id}`}>{link.title}</a>
+			</li>
+		 ))}
+		</ul>
+	 </div>
+	</nav>
+  );
+};
+
+export default Navbar;
